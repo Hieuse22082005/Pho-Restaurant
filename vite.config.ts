b574d2b239442e3779accf8128118483a1327427
Hieuse22082005 <duongxuanhieu22082005@gmail.com>
2026-09-09 23:34:35 +0700

    tiêu đề

--- vite.config.ts
+++ vite.config.ts
@@ -81,7 +81,7 @@ function figmaSiteConfiguration(config: FigmaSiteConfiguration): Plugin {
     return html.replace(`<!-- ${slotName} -->`, content)
   }
 
-  const title = config.title ?? "Figma Make App"
+  const title = config.title ?? "Phở Ngọc Hân  - Phở Gia Truyền Nam Định"
   const description = config.description ?? ''
   const favicon = config.icons?.icon ?? ''
   const socialImage = config.openGraph?.image ?? ''
